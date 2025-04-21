eec818c74aca1e06be25e68e66c214411752c201
Denis <denisfilho2@gmail.com>
2025-04-21 22:33:05 +0000

    ok

--- sobre.tsx
+++
@@ -1,8 +0,0 @@
-export default function Sobre() {
-  return (
-    <main className="p-10">
-      <h1 className="text-2xl font-bold mb-4">Sobre o Desenvolvedor</h1>
-      <p>Este aplicativo foi desenvolvido por um estudante de tecnologia, como parte de um projeto para praticar Next.js e Tailwind CSS.</p>
-    </main>
-  )
-}
