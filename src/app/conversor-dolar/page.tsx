14feaa3569116474d4b2ce8c47d54eea895815e5
Denis <denisfilho2@gmail.com>
2025-04-21 23:10:40 +0000

    Tudo ok

---
+++ src/app/conversor-dolar/page.tsx
@@ -0,0 +1,8 @@
+export default function ConversorDolar() {
+    return (
+      <div className="p-8">
+        <h1 className="text-2xl font-bold text-blue-600">Conversor de Dólar</h1>
+      </div>
+    );
+  }
+  
